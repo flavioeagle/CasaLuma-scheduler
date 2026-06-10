17c1a06272fb837ba779d2f4892aa1e429169c8c
flavioeagle <flavioeagle23@gmail.com>
2026-06-09 23:25:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef, useEffect } from "react";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js"
+
+;
 
 // ─── Supabase config ───────────────────────────────────────────────
 const SUPABASE_URL = "https://gvkkzdzfjiafpjkyscjn.supabase.co";
